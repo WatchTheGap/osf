2d4fd5410d577fe9ad7f0500575908bcc00003e0
Sara Basile <sara@snbasile.com>
2022-11-30 17:44:28 -0500

    added additional form validation, error message for duplicate email

--- src/app/signup-form/signup-form.component.ts
+++ src/app/signup-form/signup-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, TemplateRef, ViewContainerRef } from '@angular/core';
 import { FormBuilder, FormGroup, FormControl, Validators} from '@angular/forms';
 import { User } from '../user';
 import { UserService } from '../user.service';
@@ -14,14 +14,22 @@ export class SignupFormComponent implements OnInit {
 
   @ViewChild('successBtn') successBtn:any;
 
+  @ViewChild('dupeWarn', { read: TemplateRef }) dupeWarn:any;
 
-  constructor(private builder: FormBuilder, private userService: UserService) {
+
+
+  constructor(private builder: FormBuilder, private userService: UserService, private vref:ViewContainerRef) {
     this.SignupFormData = this.builder.group({
       fullname: new FormControl('', [Validators.required]),
       email: new FormControl('', [Validators.required, Validators.email]),
       //add Validators.pattern(/\/\/putRegexInHere/\/\/)
       //validators for phone
-      phone: new FormControl('', [Validators.required]),
+      phone: new FormControl('', [
+        Validators.required,
+        Validators.pattern("^[0-9]*$"),
+        Validators.minLength(10),
+        Validators.maxLength(10)]
+        ),
       dob: new FormControl('', [Validators.required]),
       instagram: new FormControl(''),
       terms: [false, Validators.requiredTrue]
@@ -32,7 +40,6 @@ export class SignupFormComponent implements OnInit {
    }
 
    triggerFalseClick() {
-    console.log('inside triggerFalseClick');
     let inputElement: HTMLElement = this.successBtn.nativeElement as HTMLElement;
     inputElement.click();
   }
@@ -47,7 +54,13 @@ export class SignupFormComponent implements OnInit {
     // Create observer object
 const myObserver = {
   next: (user: User) => console.log('Observer got a next value: ' + user),
-  error: (err: Error) => console.error('Observer got an error: ' + err.message),
+  error: (err: any) => {
+    if (err.status === 422) {
+      this.vref.createEmbeddedView(this.dupeWarn);
+
+    }
+    console.error('status', err.status);
+  },
   complete: () => {
     this.triggerFalseClick()
     this.SignupFormData.reset();
@@ -63,14 +76,11 @@ const myObserver = {
    onSubmit(data: any) {
 
     if (typeof data.dob === 'object') {
-      console.log('its an object');
       data.dob = new Date(data.dob.year, data.dob.month-1, data.dob.day);
-      console.log('after? ',data.dob, data);
       let newdata = data;
       this.addUser(newdata);
     }
     else {
-      console.log('not object?');
     }
     // this.addUser(data);
    }
